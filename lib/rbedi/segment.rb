cadf831da1bef97f7ab369505546c2b73e1cf14a
Matthias Lee <matthias@matthiasclee.com>
2024-06-18 10:35:08 -0700

    misc

--- lib/rbedi/segment.rb
+++ lib/rbedi/segment.rb
@@ -1,7 +1,8 @@
 module RBEDI
   class Segment
-    def initialize(segment_name, segment_elements = [])
+    def initialize(segment_name, segment_elements={})
       @segment_name = get_raw_segment_name(segment_name)
+      @separator = segment_elements.delete(:_separator)
 
       begin
         @codes_class = Codes.const_get(@segment_name)
@@ -9,13 +10,9 @@ module RBEDI
         @codes_class = nil
       end
 
-      if segment_elements.is_a?(Hash)
-        @segment_elements = []
-        segment_elements.each do |k, v|
-          self[k] = v
-        end
-      else
-        @segment_elements = segment_elements
+      @segment_elements = []
+      segment_elements.each do |k, v|
+        self[k] = v
       end
     end
 
@@ -31,21 +28,45 @@ module RBEDI
       raise NonExistentElementError, "Element `#{element}' does not exist for segment `#{raw_segment_name}'" unless element_pos
 
       code = @codes_class.code(element_pos, get_raw_element(element_pos))
-      code.nil? ? get_raw_element(element_pos) : code
+
+      raw_element = get_raw_element(element_pos)
+
+      if @separator
+        raw_element = raw_element.split(@separator)
+        raw_element.map! do |i|
+          c = @codes_class.code(element_pos, i)
+          c.nil? ? i : c
+        end
+
+        return raw_element unless raw_element.length == 1
+      end
+
+      code.nil? ? raw_element : code
     end
 
     def []=(element, value)
-      return set_raw_element(element, value) unless @codes_class
+      return set_raw_element(element, (value.is_a?(Array) && @separator) ? value.join(@separator) : value.to_s) unless @codes_class
 
       element_pos = get_element_pos(element)
 
       raise NonExistentElementError, "Element `#{element}' does not exist for segment `#{raw_segment_name}'" unless element_pos
 
-      return set_raw_element(element_pos, value) if value.is_a?(String)
+      if @separator && value.is_a?(Array)
+        value.map! do |i|
+          if i.is_a?(String)
+            i
+          else
+            c = @codes_class.code(element_pos, i)
+            c.nil? ? i : c
+          end
+        end
 
-      code = @codes_class.code(element_pos, value)
-
-      set_raw_element(element_pos, code.nil? ? value : code)
+        set_raw_element(element_pos, value.join(@separator))
+      else
+        return set_raw_element(element_pos, value) if value.is_a?(String)
+        code = @codes_class.code(element_pos, value)
+        set_raw_element(element_pos, code.nil? ? value : code)
+      end
     end
 
     def segment_type
@@ -64,12 +85,18 @@ module RBEDI
       @segment_elements[n - 1] = v
     end
 
-    def self.parse(segment)
-      elements = segment.split(Codes::ELEMENT_SEPARATOR)
-      new(elements[0], elements[1..-1])
+    def self.parse(segment, separator:nil)
+      elements = segment.delete_suffix("~").split(Codes::ELEMENT_SEPARATOR)
+      segment = new(elements[0])
+      elements[1..-1].each_with_index do |e, i|
+        segment.set_raw_element(i+1, e)
+      end
+      segment.separator = separator
+      return segment
     end
 
     attr_reader :segment_elements
+    attr_accessor :separator
 
     private
     
